de258b62551d67e1cc2d85a0a9e3fc4b11c286cc
Kamalika Saha <111375654+Saha-Kamalika@users.noreply.github.com>
2023-07-28 09:56:44 +0530

    Add files via upload

--- logic_files/CheckAll.java
+++ logic_files/CheckAll.java
@@ -1,1 +1,36 @@
+package junit_Testing;
+import java.util.StringTokenizer;
 
+public class CheckAll {
+	public static int Max(int a[]) {
+		int max=a[0];
+		 for(int i=1;i<a.length;i++){  
+	            if(max<a[i])  
+	                max=a[i];  
+	        }  
+	        return max;  
+	    }
+	public static int square(int n) {
+		return n*n;
+	}
+	public static boolean isPrime(int n) {
+		if(n==1) return false;
+		for(int i=2;i<=n/2;i++) {
+			if(n%i==0) return false;
+		}
+		return true;
+	}
+	public static String reverse(String str) {
+		StringBuilder ans=new StringBuilder();
+		StringTokenizer s=new StringTokenizer(str," ");
+		while(s.hasMoreTokens()) {
+			StringBuilder string=new StringBuilder();
+			string.append(s.nextToken());
+			string.reverse();
+			
+			ans.append(string);
+			ans.append(" ");
+		}
+		return ans.toString();
+	}
+}
